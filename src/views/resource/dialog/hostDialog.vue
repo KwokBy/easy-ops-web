39e9c8f1e6c7ab5627bf9f0022b0524d2112d3a2
doubleguo <1042936527@qq.com>
2022-03-16 23:06:44 +0800

    add host manager init

---
+++ src/views/resource/dialog/hostDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-button type="primary" :icon="Plus" @click="dialogVisible = true"
+    >新建
+  </el-button>
+  <el-dialog v-model="dialogVisible" title="Tips" width="30%" draggable>
+    <HostForm />
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >Confirm</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HostDialog"
+};
+</script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { ElDialog } from "element-plus";
+import { Plus } from "@element-plus/icons-vue";
+import HostForm from "/@/views/resource/form/hostForm.vue";
+const dialogVisible = ref(false);
+</script>
--- src/views/resource/host.vue
+++ src/views/resource/host.vue
@@ -1,7 +1,96 @@
-<template><div>host</div></template>
+<template>
+  <el-card>
+    <HostDialog />
+    <el-table
+      :data="filterTableData"
+      style="width: 100%"
+      border
+      :header-cell-style="{ background: '#FAFAFA' }"
+    >
+      <el-table-column
+        v-for="(item, index) in tableDataLabel"
+        :key="index"
+        :label="item.label"
+        :prop="item.prop"
+        :align="item.align"
+      />
+      <el-table-column label="操作" align="center">
+        <template #default="scope">
+          <el-button
+            size="small"
+            type="text"
+            @click="handleEdit(scope.$index, scope.row)"
+            >Edit</el-button
+          >
+          <el-button
+            size="small"
+            type="text"
+            @click="handleDelete(scope.$index, scope.row)"
+            >Delete</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
+</template>
 
 <script lang="ts">
 export default {
   name: "host"
 };
+</script>
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import { ElButton, ElTable, ElTableColumn, ElCard } from "element-plus";
+import HostDialog from "/@/views/resource/dialog/hostDialog.vue";
+interface Host {
+  id: number;
+  host: string;
+  host_name: string;
+  username: string;
+  name: string;
+}
+interface Table {
+  label: string;
+  prop: string;
+  align: string;
+}
+const tableDataLabel: Table[] = [
+  {
+    label: "主机名称",
+    prop: "host_name",
+    align: "center"
+  },
+  {
+    label: "IP地址",
+    prop: "host",
+    align: "center"
+  },
+  {
+    label: "配置信息",
+    prop: "info",
+    align: "center"
+  },
+  {
+    label: "状态",
+    prop: "status",
+    align: "center"
+  }
+];
+const search = ref("");
+const filterTableData = computed(() =>
+  tableData.filter(
+    data =>
+      !search.value ||
+      data.name.toLowerCase().includes(search.value.toLowerCase())
+  )
+);
+const handleEdit = (index: number, row: Host) => {
+  console.log(index, row);
+};
+const handleDelete = (index: number, row: Host) => {
+  console.log(index, row);
+};
+
+const tableData: Host[] = [];
 </script>
