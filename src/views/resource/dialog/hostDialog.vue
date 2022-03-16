a8fa433bf94d383d3787a6c502104703890c55eb
doubleguo <1042936527@qq.com>
2022-03-17 01:17:46 +0800

    表单格式

--- src/views/resource/dialog/hostDialog.vue
+++ src/views/resource/dialog/hostDialog.vue
@@ -2,16 +2,16 @@
   <el-button type="primary" :icon="Plus" @click="dialogVisible = true"
     >新建
   </el-button>
-  <el-dialog v-model="dialogVisible" title="Tips" width="30%" draggable>
+  <el-dialog v-model="dialogVisible" title="Tips" width="50%" draggable>
     <HostForm />
-    <template #footer>
+    <!-- <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
         <el-button type="primary" @click="dialogVisible = false"
           >Confirm</el-button
         >
       </span>
-    </template>
+    </template> -->
   </el-dialog>
 </template>
 
--- src/views/resource/form/hostForm.vue
+++ src/views/resource/form/hostForm.vue
@@ -1,69 +1,42 @@
 <template>
   <el-form
-    ref="ruleFormRef"
-    :model="ruleForm"
+    ref="hostFormRef"
+    :model="hostForm"
     :rules="rules"
     label-width="120px"
-    class="demo-ruleForm"
+    class="demo-hostForm"
   >
-    <el-form-item label="Activity name" prop="name">
-      <el-input v-model="ruleForm.name" />
+    <el-form-item label="主机名称" prop="name">
+      <el-input v-model="hostForm.name" />
     </el-form-item>
-    <el-form-item label="Activity zone" prop="region">
-      <el-select v-model="ruleForm.region" placeholder="Activity zone">
-        <el-option label="Zone one" value="shanghai" />
-        <el-option label="Zone two" value="beijing" />
-      </el-select>
+    <el-form-item label="连接地址" prop="desc">
+      <el-col :span="7"
+        ><el-input placeholder="用户名" v-model="hostForm.desc"
+          ><template #prepend>ssh</template></el-input
+        ></el-col
+      >
+      <el-col :span="10"
+        ><el-input placeholder="主机名/IP" v-model="hostForm.desc"
+          ><template #prepend>@</template></el-input
+        ></el-col
+      >
+      <el-col :span="7"
+        ><el-input placeholder="端口" v-model="hostForm.desc"
+          ><template #prepend>-p</template></el-input
+        ></el-col
+      >
     </el-form-item>
-    <el-form-item label="Activity time" required>
-      <el-col :span="11">
-        <el-form-item prop="date1">
-          <el-date-picker
-            v-model="ruleForm.date1"
-            type="date"
-            placeholder="Pick a date"
-            style="width: 100%"
-          />
-        </el-form-item>
-      </el-col>
-      <el-col class="text-center" :span="2">
-        <span class="text-gray-500">-</span>
-      </el-col>
-      <el-col :span="11">
-        <el-form-item prop="date2">
-          <el-time-picker
-            v-model="ruleForm.date2"
-            placeholder="Pick a time"
-            style="width: 100%"
-          />
-        </el-form-item>
-      </el-col>
+    <el-form-item label="主机密码" prop="name">
+      <el-input v-model="hostForm.name" />
     </el-form-item>
-    <el-form-item label="Instant delivery" prop="delivery">
-      <el-switch v-model="ruleForm.delivery" />
-    </el-form-item>
-    <el-form-item label="Activity type" prop="type">
-      <el-checkbox-group v-model="ruleForm.type">
-        <el-checkbox label="Online activities" name="type" />
-        <el-checkbox label="Promotion activities" name="type" />
-        <el-checkbox label="Offline activities" name="type" />
-        <el-checkbox label="Simple brand exposure" name="type" />
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="Resources" prop="resource">
-      <el-radio-group v-model="ruleForm.resource">
-        <el-radio label="Sponsorship" />
-        <el-radio label="Venue" />
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="Activity form" prop="desc">
-      <el-input v-model="ruleForm.desc" type="textarea" />
+    <el-form-item label="备注信息" prop="desc">
+      <el-input v-model="hostForm.desc" type="textarea" />
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm(ruleFormRef)"
+      <el-button type="primary" @click="submitForm(hostFormRef)"
         >Create</el-button
       >
-      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+      <el-button @click="resetForm(hostFormRef)">Reset</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -75,24 +48,12 @@ export default {
 
 <script lang="ts" setup>
 import { reactive, ref } from "vue";
-import {
-  ElForm,
-  ElFormItem,
-  ElButton,
-  ElInput,
-  ElCheckbox
-} from "element-plus";
+import { ElForm, ElFormItem, ElButton, ElInput } from "element-plus";
 
 type FormInstance = InstanceType<typeof ElForm>;
-const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive({
-  name: "Hello",
-  region: "",
-  date1: "",
-  date2: "",
-  delivery: false,
-  type: [],
-  resource: "",
+const hostFormRef = ref<FormInstance>();
+const hostForm = reactive({
+  name: "",
   desc: ""
 });
 
@@ -100,44 +61,6 @@ const rules = reactive({
   name: [
     { required: true, message: "Please input Activity name", trigger: "blur" },
     { min: 3, max: 5, message: "Length should be 3 to 5", trigger: "blur" }
-  ],
-  region: [
-    {
-      required: true,
-      message: "Please select Activity zone",
-      trigger: "change"
-    }
-  ],
-  date1: [
-    {
-      type: "date",
-      required: true,
-      message: "Please pick a date",
-      trigger: "change"
-    }
-  ],
-  date2: [
-    {
-      type: "date",
-      required: true,
-      message: "Please pick a time",
-      trigger: "change"
-    }
-  ],
-  type: [
-    {
-      type: "array",
-      required: true,
-      message: "Please select at least one activity type",
-      trigger: "change"
-    }
-  ],
-  resource: [
-    {
-      required: true,
-      message: "Please select activity resource",
-      trigger: "change"
-    }
   ],
   desc: [
     { required: true, message: "Please input activity form", trigger: "blur" }
